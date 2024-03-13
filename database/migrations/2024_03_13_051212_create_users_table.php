83b00bf18904352c151c7b471e6604ba0fc257bb
Mico Winaryo Dwi Putranto <budakbagus0@gmail.com>
2024-03-13 12:49:01 +0700

    Make Table User
    Membuat tablel user dan juga seedernya

--- database/migrations/2024_03_13_051212_create_users_table.php
+++ database/migrations/2024_03_13_051212_create_users_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('email', 50)->unique();
+            $table->bigInteger('mobile_number');
+            $table->string('password', 30);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('users');
     }
 };
